26164daff94c13190c1311d56a2de6536ffaddb7
Fabio <fabio.martel@vallegrande.edu.pe>
2024-11-17 21:14:37 -0500

    v1

---
+++ src/main/java/pe/edu/vallegrande/vg_ms_attorney/application/webClient/AuthServiceClient.java
@@ -0,0 +1,28 @@
+package pe.edu.vallegrande.vg_ms_attorney.application.webClient;
+
+import com.google.common.net.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import pe.edu.vallegrande.vg_ms_attorney.domain.dto.TokenValidationResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthServiceClient {
+
+    private final WebClient webClient;
+
+    @Autowired
+    public AuthServiceClient(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://vg-ms-user-production.up.railway.app/firebase-users").build();
+    }
+
+    public Mono<TokenValidationResponse> validateToken(String token) {
+        return webClient.get()
+                .uri("/validate")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .retrieve()
+                .bodyToMono(TokenValidationResponse.class);
+    }
+
+}
